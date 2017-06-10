c964694e8b4da78db28cca0d5c6c41e280bac4e0
agility1 <agility1@ssdrs2.layerip.com>
2017-06-10 10:02:09 +0000

    updating github with live site fixes

--- plugin-agility-shows/agility-show-widgets.php
+++ plugin-agility-shows/agility-show-widgets.php
@@ -23,54 +23,54 @@ class FutureShows_Widget extends WP_Widget {
 
 		echo $before_widget;
 		echo $before_title.'Coming Soon'.$after_title;
-
+		
 		// the query
 		$today = date('Ymd');
-
+		
 		$args = array (
-				'post_type'	=> 'shows',
-				'post_status'	=> array('pending'),
-				'meta_key'	=> 'start_date',
-				'orderby'	=> 'meta_value',
-				'order'		=> 'ASC',
-				'numberposts'	=> 50,
-				'meta_query' 	=> array(
-						array(
-								'key'		=> 'end_date',
-								'compare'	=> '>=',
-								'value'		=> $today,
-						),
-				)
+			'post_type'	=> 'shows',
+			'post_status'	=> array('pending'),
+			'meta_key'	=> 'start_date',
+			'orderby'	=> 'meta_value',
+			'order'		=> 'ASC',
+			'numberposts'	=> 50,
+			'meta_query' 	=> array(
+				array(
+					'key'		=> 'end_date',
+					'compare'	=> '>=',
+					'value'		=> $today,
+				),
+			)
 		);
-
+		
 		// get posts
 		$posts = get_posts($args);
 		global $post;
-
+		
 		// loop
-		if( $posts ) {
+		if( $posts ) {	
 			echo '<ul style="font-size:0.9em" class="agility-show-widget list-unstyled">';
-			foreach( $posts as $post ) {
+			foreach( $posts as $post ) {		
 				setup_postdata( $post );
-				$date = new DateTime(get_field('start_date', false, false));
-				echo '<li>' . get_the_title() .' ('.$date->format('jS M Y').')</li>';
+                $date = new DateTime(get_field('start_date', false, false));
+				echo '<li>' . get_the_title() .' ('.$date->format('jS M Y').')</li>';		
 			}
-			echo '</ul>';
+			echo '</ul>';		
 			wp_reset_postdata();
-
+		
 		} else {
 			// no posts found
 			echo 'No future shows currently on the system';
 		}
-
+		
 		echo $after_widget;
 	}
-
+	
 	function update($new_instance, $old_instance) {
 		$instance = $old_instance;
 		return $instance;
 	}
-
+	
 	function form($instance) {
 	}
 }
@@ -90,55 +90,55 @@ class LiveShows_Widget extends WP_Widget {
 
 		echo $before_widget;
 		echo $before_title.'Live Shows'.$after_title;
-
+		
 		// the query
 		$today = date('Ymd');
-
+		
 		$args = array (
-				'post_type'	=> 'shows',
-				'post_status'	=> array('publish'),
-				'meta_key'	=> 'start_date',
-				'orderby'	=> 'meta_value',
-				'order'		=> 'ASC',
-				'numberposts'	=> 20,
-				'meta_query' 	=> array(
-						array(
-								'key'		=> 'close_date',
-								'compare'	=> '>=',
-								'value'		=> $today,
-						),
-				)
+			'post_type'	=> 'shows',
+			'post_status'	=> array('publish'),
+			'meta_key'	=> 'start_date',
+			'orderby'	=> 'meta_value',
+			'order'		=> 'ASC',
+			'numberposts'	=> 20,
+			'meta_query' 	=> array(
+				array(
+					'key'		=> 'close_date',
+					'compare'	=> '>=',
+					'value'		=> $today,
+				),
+			)
 		);
-
+		
 		// get posts
 		$posts = get_posts($args);
 		global $post;
-
+		
 		// loop
-		if( $posts ) {
-			echo '<ul style="font-size:0.9em" class="agility-show-widget list-unstyled">';
-			foreach( $posts as $post ) {
+		if( $posts ) {	
+			echo '<ul style="font-size:0.9em" class="agility-show-widget list-unstyled">';	
+			foreach( $posts as $post ) {		
 				setup_postdata( $post );
-				$date = new DateTime(get_field('start_date', false, false));
-				echo '<li><a href="' . get_the_permalink() .'" rel="bookmark">' . get_the_title() .' ('.$date->format('jS M').')</a></li>';
+                $date = new DateTime(get_field('start_date', false, false));
+				echo '<li><a href="' . get_the_permalink() .'" rel="bookmark">' . get_the_title() .' ('.$date->format('jS M').')</a></li>';		
 			}
-			echo '</ul>';
+			echo '</ul>';		
 			wp_reset_postdata();
-
+		
 		} else {
 			// no posts found
 			echo 'No shows are currently available to enter.';
 		}
-
+		
 		echo $after_widget;
 
 	}
-
+	
 	function update($new_instance, $old_instance) {
 		$instance = $old_instance;
 		return $instance;
 	}
-
+	
 	function form($instance) {
 	}
 }
