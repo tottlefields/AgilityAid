1f8a92d271ad44c7547b3c0cbcabe881363cbb2c
Ellen Schofield <agility1@ssdrs2.layerip.com>
2017-06-19 11:54:10 +0000

    added custom user columns for shows/entries

--- ppd-child/functions.php
+++ ppd-child/functions.php
@@ -1,4 +1,70 @@
 <?php
+add_action('manage_users_columns','agilityaid_manage_users_columns');
+function agilityaid_manage_users_columns($column_headers) {
+    unset($column_headers['posts']);
+    $column_headers['custom_posts'] = 'Entries';
+    return $column_headers;
+}
+
+add_action('manage_users_custom_column','agilityaid_manage_users_custom_column',10,3);
+function agilityaid_manage_users_custom_column($custom_column,$column_name,$user_id) {
+    if ($column_name=='custom_posts') {
+        $counts = _agilityaid_get_author_post_type_counts();
+        $custom_column = array();
+        if (isset($counts[$user_id]) && is_array($counts[$user_id]))
+            foreach($counts[$user_id] as $count) {
+                $link = admin_url() . "edit.php?post_type=" . $count['type']. "&author=".$user_id;
+                // admin_url() . "edit.php?author=" . $user->ID;
+                $custom_column[] = "\t<tr><th><a href={$link}>{$count['label']}</a></th><td>{$count['count']}</td></tr>";
+            }
+        $custom_column = implode("\n",$custom_column);
+        if (empty($custom_column))
+            $custom_column = "<th>[none]</th>";
+        $custom_column = "<table>\n{$custom_column}\n</table>";
+    }
+    return $custom_column;
+}
+
+function _agilityaid_get_author_post_type_counts() {
+    static $counts;
+    if (!isset($counts)) {
+        global $wpdb;
+        global $wp_post_types;
+        $sql = <<<SQL
+        SELECT
+        post_type,
+        post_author,
+        COUNT(*) AS post_count
+        FROM
+        {$wpdb->posts}
+        WHERE 1=1
+        AND post_type IN ('entries','shows')
+        AND post_status IN ('publish','pending', 'draft')
+        GROUP BY
+        post_type,
+        post_author
+SQL;
+        $posts = $wpdb->get_results($sql);
+        foreach($posts as $post) {
+            $post_type_object = $wp_post_types[$post_type = $post->post_type];
+            if (!empty($post_type_object->label))
+                $label = $post_type_object->label;
+            else if (!empty($post_type_object->labels->name))
+                $label = $post_type_object->labels->name;
+            else
+                $label = ucfirst(str_replace(array('-','_'),' ',$post_type));
+            if (!isset($counts[$post_author = $post->post_author]))
+                $counts[$post_author] = array();
+            $counts[$post_author][] = array(
+                'label' => $label,
+                'count' => $post->post_count,
+                'type' => $post->post_type,
+                );
+        }
+    }
+    return $counts;
+}
+
 function my_theme_enqueue_styles() {
  
     $parent_style = 'ppd-bootstrap-basic'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
