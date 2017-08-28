b7628c232375048a5c6616072af3745354b35ba5
Ellen Schofield <tottlefields@gmail.com>
2017-08-28 12:08:44 +0100

    added extra handlers

--- ppd-bootstrap-basic/page-templates/my-details.php
+++ ppd-bootstrap-basic/page-templates/my-details.php
@@ -1,4 +1,34 @@
 <?php /* Template Name: My Details */ ?>
+<?php 
+if(!is_user_logged_in()) {
+	wp_redirect(site_url('/login/'));
+	exit;
+}
+
+global $current_user, $wpdb;
+get_currentuserinfo();
+
+$userId = $current_user->ID;
+$user_meta = get_user_meta( $userId );
+if(!isset($user_meta['user_ref'])){
+	$ref = sprintf('%04d', $userId);
+	update_user_meta($userId, 'user_ref', 'AA'.$ref);
+	$user_meta['user_ref'] = 'AA'.$ref;
+}
+
+$handlers = array();
+if(isset($user_meta['handlers'][0])){
+	$handlers = unserialize($user_meta['handlers'][0]);
+}
+
+if (isset($_POST['add_handler']) && $_POST['add_handler'] == 'submit'){
+	array_push($handlers, $_POST['new_handler']);
+	$uiq_handlers = array_unique($handlers);
+	update_user_meta($userId, 'handlers', $uiq_handlers);	
+}
+asort($handlers);
+
+?>
 <?php get_header(); ?>
 
 <div id="content" class="standard">
@@ -20,7 +50,23 @@
             <div class="col-md-6">
 				<a href="/account/" class="btn btn-info pull-right">My Account</a><h1>&nbsp;</h1>
 				<h4>Extra Handlers</h4>
-            </div>
+				<?php 
+				
+				if (count($handlers) > 0){
+					echo '<ul>';
+					foreach ($handlers as $h){
+						echo '<li>'.$h.'</li>';
+					}
+					echo '</ul>';
+				}
+				?>
+				<form method="post" class="form-inline">
+					<div class="form-group">
+						<input class="form-control" type="text" name="new_handler" id="new_handler" placeholder="Handler Name" />
+					</div>
+					<button type="submit" class="btn btn-primary" name="add_handler" id="add_handler" value="submit">Add Handler</button>
+				</form>
+			</div>
         </div>
     </div>
 </div>
